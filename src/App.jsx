9fe89f5be3abd8bbf904d63afdfcd18bea7e0102
Nguyen Trung Kien <kient2005@gmail.com>
2022-10-22 09:44:13 +0700

    message

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,15 @@ import Test from "./pages/Test";
 function App() {
   return (
     <Routes>
-      <Route index path="/" element={<Home />} />
-      <Route  path="/test" element={<Test />} />
-      <Route element={<SinglePage />} path="/movie/:movieId" />
-      <Route element={<SinglePageTV />} path="/tv/:movieId" />
-      <Route element={<Movie />} path="/newMovies" />
-      <Route element={<TVShows />} path="/newTVShows" />
-      <Route element={<PopularMovies />} path="/polularMovies" />
-      <Route element={<PopularTV />} path="/popularTVShows" />
-      <Route element={<TopRated />} path="/TopRated" />
+      <Route index path="/" element={<Home />}/>
+      <Route  path="/test" element={<Test />}/>
+      <Route element={<SinglePage />} path="/movie/:movieId"/>
+      <Route element={<SinglePageTV />} path="/tv/:movieId"/>
+      <Route element={<Movie />} path="/newMovies"/>
+      <Route element={<TVShows />} path="/newTVShows"/>
+      <Route element={<PopularMovies />} path="/polularMovies"/>
+      <Route element={<PopularTV />} path="/popularTVShows"/>
+      <Route element={<TopRated />} path="/TopRated"/>
     </Routes>
   );
 }
